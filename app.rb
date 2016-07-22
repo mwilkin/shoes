f2f43564c18ab5684c04ff4c690fa247730798da
Matt Wilkin <mwilkin22@gmail.com>
2016-07-22 10:49:08 -0700

    add spec and code for allowing a user to add a new brand.

--- app.rb
+++ app.rb
@@ -7,3 +7,14 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   erb(:index)
 end
+
+get('/brands') do
+  @brands = Brand.all()
+  erb(:brands)
+end
+
+post('/brands') do
+  name = params.fetch('brand_name')
+  new_brand = Brand.create({:name => name})
+  redirect('/brands')
+end
