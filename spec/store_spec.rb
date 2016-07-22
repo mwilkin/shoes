6dedf88db8ec1b97ac4f8b0f30b0b9676f6502bb
Matt Wilkin <mwilkin22@gmail.com>
2016-07-22 10:15:28 -0700

    add spec and code for Store has many brands.

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -1,1 +1,14 @@
+require('spec_helper')
 
+describe(Store) do
+  it('has many brands') do
+    test_store = Store.create({:name => "Fleet Feet"})
+    test_brand1 = Brand.create({:name => "Altra"})
+    test_brand2 = Brand.create({:name => "Saucony"})
+    test_brand3 = Brand.create({:name => "Nike"})
+    test_store.brands.push(test_brand1)
+    test_store.brands.push(test_brand2)
+    test_store.brands.push(test_brand3)
+    expect(test_store.brands()).to(eq([test_brand1, test_brand2, test_brand3]))
+  end
+end
