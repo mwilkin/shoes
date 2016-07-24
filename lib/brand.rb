16856618197caf6c93441db1d89c510c0a17c081
Matt Wilkin <mwilkin22@gmail.com>
2016-07-24 12:16:12 -0700

    add uniqueness validation for Brand name.

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,7 +1,7 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
   before_save(:titleize_name)
-  validates(:name, :presence => true)
+  validates(:name, {:presence => true, :uniqueness => true})
 
   private
     define_method(:titleize_name) do
